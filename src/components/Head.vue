1689760218ccff3d39d4136f0c7823c39bfc21c3
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-05 16:28:50 -0600

    Update Head.vue

--- src/components/Head.vue
+++ src/components/Head.vue
@@ -2,11 +2,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="icon" href="/apple.png">
 	<link rel="stylesheet" href="/styles.css">
-	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&family=PT+Sans&family=Poppins:wght@600&family=Source+Code+Pro&display=swap" rel="stylesheet">
 	<meta charset="UTF-8">
 	<meta name="author" content="illogicalapple">
-	<meta name="description" content="{{ description }}">
+	<meta name="description" content={{ description }}>
 	<title>{{ title }}</title>
 </template>
 
