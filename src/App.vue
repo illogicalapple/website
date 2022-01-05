67b5364dd35efaa159cc0606611d1856ff76b358
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-05 10:24:31 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+	<head>
+		<Head title="illogicalapple" description="illogicalapple's website"></Head>
+		<style>
+			@media screen and (max-width: 100vh) { /* when the width is less than the height */
+				img.apple {
+					display: none;
+				}
+				#content div.text {
+					width: 80vw !important; /* inline styles were a mistake */
+				}
+			}
+		</style>
+	</head>
+	<body style="margin: 0px;">
+		<Navbar page="home"></Navbar>
+		<main id="content">
+			<div class="text" style="position: absolute; transform: translateY(-50%); top: calc(50% + 50px); left: 10vw; width: calc(80vw - (100vh - 50px) / 1.509765625); font-size: 1.4em;">
+				<h1 class="red"> Hello. </h1>
+				<p style="padding: 0px; margin: 0px;">
+					My name is <span class="red">
+						illogicalapple
+					</span> and I am an <span class="red">
+						apple. 
+					</span>
+					I started making <a href="true-facts" style="color: var(--orange);">true-facts</a> in <time datetime="2020-12">
+						December 2020,
+					</time> and I made some other fun <a href="my-stuff" style="color: var(--orange);">stuff</a> later.
+					I really like making stuff.
+					<br> <br> Hope you enjoy!
+				</p>
+			</div>
+			<img src="apple-image.jpg" style="position: absolute; bottom: 0px; right: 0px; height: calc(100vh - 50px); z-index: -1;" class="apple" alt="Apple">
+		</main>
+	</body>
 </template>
 
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
