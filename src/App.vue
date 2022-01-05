64b4125352acbb4227105a26bc3adcc58f256bd4
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-05 16:34:18 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-	<head>
-		<Head title="illogicalapple" description="illogicalapple's website"></Head>
-	</head>
-	<body style="margin: 0px;">
-		<Navbar page="home"></Navbar>
-		<main id="content">
-			<div class="text" style="position: absolute; transform: translateY(-50%); top: calc(50% + 50px); left: 10vw; width: calc(80vw - (100vh - 50px) / 1.509765625); font-size: 1.4em;">
-				<h1 class="red"> Hello. </h1>
-				<p style="padding: 0px; margin: 0px;">
-					My name is <span class="red">
-						illogicalapple
-					</span> and I am an <span class="red">
-						apple. 
-					</span>
-					I started making <a href="true-facts" style="color: var(--orange);">true-facts</a> in <time datetime="2020-12">
-						December 2020,
-					</time> and I made some other fun <a href="my-stuff" style="color: var(--orange);">stuff</a> later.
-					I really like making stuff.
-					<br> <br> Hope you enjoy!
-				</p>
-			</div>
-			<img src="/apple-image.jpg" style="position: absolute; bottom: 0px; right: 0px; height: calc(100vh - 50px); z-index: -1;" class="apple" alt="Apple">
-		</main>
-	</body>
+	<Head title="illogicalapple" description="illogicalapple's website"></Head>
+	<Navbar page="home"></Navbar>
+	<main id="content">
+		<div class="text" style="position: absolute; transform: translateY(-50%); top: calc(50% + 50px); left: 10vw; width: calc(80vw - (100vh - 50px) / 1.509765625); font-size: 1.4em;">
+			<h1 class="red"> Hello. </h1>
+			<p style="padding: 0px; margin: 0px;">
+				My name is <span class="red">
+					illogicalapple
+				</span> and I am an <span class="red">
+					apple. 
+				</span>
+				I started making <a href="true-facts" style="color: var(--orange);">true-facts</a> in <time datetime="2020-12">
+					December 2020,
+				</time> and I made some other fun <a href="my-stuff" style="color: var(--orange);">stuff</a> later.
+				I really like making stuff.
+				<br> <br> Hope you enjoy!
+			</p>
+		</div>
+		<img src="/apple-image.jpg" style="position: absolute; bottom: 0px; right: 0px; height: calc(100vh - 50px); z-index: -1;" class="apple" alt="Apple">
+	</main>
 </template>
 
 <script setup>
