37b8ce75318a168e9bede0fac57545c466589ac8
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-06 16:10:32 -0600

    Create routes.js

---
+++ src/routes.js
@@ -0,0 +1,27 @@
+export let routes = [
+	{
+		path: "/",
+	 	component: Home,
+	 	meta: {
+			title: "illogicalapple",
+			metaTags: [
+				{
+					name: "description",
+					content: "my website"
+				},
+				{
+					name: "viewport",
+					content: "width=device-width, initial-scale=1.0"
+				},
+				{
+					name: "author",
+					content: "illogicalapple"
+				},
+				{
+					name: "charset",
+					content: "UTF-8"
+				}
+			]
+		}
+	}
+]
