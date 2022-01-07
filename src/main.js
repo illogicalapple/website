7eb4d51c4e2971c359c4638554b2ec79e9573662
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-07 15:49:14 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 let app = createApp(App)
 let router = createRouter({
 	history: createWebHistory(),
+	mode: "history",
 	routes: import.meta.hot ? [] : routes
 })
 
