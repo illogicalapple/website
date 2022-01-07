4eebf2eef0fe49997421e44ada4f6a4e2ac4c92b
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-07 14:21:56 -0600

    Update Menu.vue

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,16 +1,16 @@
 <template>
 	<div class="menu" style="text-align: left; padding: 1.5em 3em; overflow-y: auto; overflow-x: hidden;">
 		<a href="/true-facts">
-			<h2 style="margin-bottom: 0.5em;color: var(--secondary);" class="blue">True Facts</h2>
-			True facts about everything.
+			<h2 style="margin-bottom: 0.5em;color: var(--secondary);" class="blue">true facts</h2>
+			true facts about everything
 		</a> <br><br>
 		<a href="/scribble">
-			<h2 style="margin-bottom: 0.5em;color: var(--secondary);" class="blue">Scribble</h2>
-			Ruins your drawings.
+			<h2 style="margin-bottom: 0.5em;color: var(--secondary);" class="blue">scribble</h2>
+			ruins your drawings
 		</a> <br><br>
 		<a href="/car-game">
-			<h2 style="margin-bottom: 0.5em;color: var(--secondary);" class="blue">Car Game</h2>
-			Vroom. Vroom. Boom.
+			<h2 style="margin-bottom: 0.5em;color: var(--secondary);" class="blue">car game</h2>
+			vroom vroom boom
 		</a>
 	</div>
 </template>
