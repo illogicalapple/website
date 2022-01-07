28de22c5b166f6d0bc0b2decaad89e5670adfc6e
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-07 14:24:58 -0600

    Create 404.vue

---
+++ src/views/404.vue
@@ -0,0 +1,13 @@
+<template>
+	<Navbar></Navbar>
+	<main id="content">
+		<div style="text-align: center; transform: translateY(-50%); position: absolute; top: 50%;">
+			<h1 style="font-size: 8em;">404</h1>
+			<span style="font-size: 1.4em;">evil kumquats ate this page</span>
+		</div>
+		<img src="/burp.png" style="position: absolute; right: 0px; bottom: 0px; transform: translate(-100%, -100%)">
+	</main>
+</template>
+<script>
+	import Navbar from "../components/Navbar.vue";
+</script>
