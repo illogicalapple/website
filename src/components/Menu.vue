565b3c1b78b429d8552d24675acaccb093aa776b
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-07 10:02:34 -0600

    Update Menu.vue

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,5 +1,16 @@
 <template>
-	<div class="menu"></div>
+	<div class="menu" data-v-21dc7603="" data-v-337b3a89 style="text-align: left; padding: 1.5em 3em;">
+		<a href="/true-facts">
+			<h2 style="margin-bottom: 0.5em;color: var(--secondary);" class="blue">True Facts</h2>
+			True facts about everything.
+		</a> <br><br>
+		<a href="/scribble">
+			<h2 style="margin-bottom: 0.5em;color: var(--secondary);" class="blue">Scribble</h2>
+			Ruins your drawings.
+		</a> <br><br>
+		<a href="/car-game">
+			<h2 style="margin-bottom: 0.5em;color: var(--secondary);" class="blue">Car Game</h2>
+			Vroom. Vroom. Boom.
+		</a>
+	</div>
 </template>
-<style scoped>
-</style>
