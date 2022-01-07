c8f7258f97fa4c9b368394eda4699b55a2900a43
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-07 09:33:57 -0600

    Update stuff.js

--- public/stuff.js
+++ public/stuff.js
@@ -1,15 +1,13 @@
 addEventListener("load", function() {
-	if(localStorage.darkMode === "true") {
-		document.body.setAttribute("data-dark-mode", "");
-		document.querySelector(".darkModeToggle").setAttribute("src", "/sun.svg");
-	}
-	document.querySelector("nav#navigation img").addEventListener("click", function() {
-		if(this.getAttribute("src") === "/menu.svg") {
-			this.src = "/close.svg";
-			document.querySelector("nav#navigation div.menu").style.display = "";
+	document.querySelector("nav#navigation a.option.stuff-option").addEventListener("click", function() {
+		let menu = document.querySelector("#navigation div.menu");
+		if(menu.hasAttribute("data-open")) {
+			menu.removeAttribute("data-open");
+			this.classList.remove("active");
 		} else {
-			this.src = "/menu.svg";
-			document.querySelector("nav#navigation div.menu").style.display = "none";
+			menu.setAttribute("data-open", "bob");
+			this.classList.add("active");
 		}
+		// BOBbBbB
 	});
 });
