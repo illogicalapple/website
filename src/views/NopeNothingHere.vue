5974303850c5dc0b15a682079ad44851561ab072
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-08 15:33:30 -0600

    Update NopeNothingHere.vue

--- src/views/NopeNothingHere.vue
+++ src/views/NopeNothingHere.vue
@@ -2,7 +2,7 @@
 	<Navbar></Navbar>
 	<main id="content">
 		<div style="text-align: center; transform: translateY(-50%); position: absolute; top: 50%; width: 100%;">
-			<h1 style="font-size: 20vmin; background: url(/apples.png); background-clip: text; -webkit-background-clip: text; color: transparent;">404</h1>
+			<h1 style="font-size: 20vmin; background: url(/apples.png); background-clip: text; -webkit-background-clip: text; color: transparent; background-position: center 20px;">404</h1>
 			<span style="font-size: 1.4em;">evil kumquats ate this page</span>
 		</div>
 		<img src="/burp.png" style="position: absolute; right: 0px; bottom: 0px; transform: translate(-20%, -20%)">
