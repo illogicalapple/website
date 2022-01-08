85048828125a808885e5635adb5268ea1b2b28e2
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-08 16:44:14 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,18 @@
 <template>
 	<router-view />
 </template>
+<script setup>
+	import { ref } from "vue";
+	export default {
+		setup() {
+			const menuOpen = ref(false);
+			const toggleMenu = () => {
+				menuOpen.value = !menuOpen.value;
+			};
+			return {
+				menuOpen,
+				toggleMenu
+			}
+		}
+	};
+</script>
