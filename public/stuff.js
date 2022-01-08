947e8fe167418b7507fabc3a7703f2cd2ffd9ead
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-08 15:48:15 -0600

    Update stuff.js

--- public/stuff.js
+++ public/stuff.js
@@ -1,13 +1,3 @@
 addEventListener("load", function() {
-	document.querySelector("nav#navigation a.option.stuff-option").addEventListener("click", function() {
-		let menu = document.querySelector("#navigation div.menu");
-		if(menu.hasAttribute("data-open")) {
-			menu.removeAttribute("data-open");
-			this.classList.remove("active");
-		} else {
-			menu.setAttribute("data-open", "bob");
-			this.classList.add("active");
-		}
-		// BOBbBbB
-	});
+	//dont use this only runs when page is initially loaded
 });
