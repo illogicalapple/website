b2ea820e2b993dde12d5fb33c3cb9f380660d818
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-08 16:14:30 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue'
 import { routes } from './routes.js'
 import { createRouter, createWebHistory } from 'vue-router'
 
-let app = createApp(App);
+let app = createApp({
+	data: {
+		menuOpen: false;
+	}
+});
 let router = createRouter({
 	history: createWebHistory(),
 	routes: import.meta.hot ? [] : routes
