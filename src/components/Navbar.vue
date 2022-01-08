5de66602afaac4e3ab3abe59a48e1686351370e6
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-08 15:07:54 -0600

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,23 +1,23 @@
 <template>
 	<nav id="navigation" style="position: fixed; left: 0px; top: 0px; height: 85px; background-color: white; border-bottom: 1px solid lightgray;" class="full-width" >
 		<div style="position: absolute; left: 10px; width: 50px;" class="relative-full-height">
-			<a style="position: absolute; left: 0px; padding: 0px 20px; display: table;" class="relative-full-height title bold float-left option nope">
+			<router-link style="position: absolute; left: 0px; padding: 0px 20px; display: table;" class="relative-full-height title bold float-left option nope">
 				<span style="display: table-cell; vertical-align: middle; text-align: center;">
 					illogical<span class="blue" style="color: var(--secondary);">apple</span>
 				</span>
-			</a>
+			</router-link>
 		</div>
 		<div style="position: absolute; right: 10px; top: 0px;" class="relative-full-height">
-			<a href="javascript:void(0)" style="display: table; padding: 0px 20px; cursor: pointer;" class="relative-full-height float-right option stuff-option">
+			<router-link href="javascript:void(0)" style="display: table; padding: 0px 20px; cursor: pointer;" class="relative-full-height float-right option stuff-option">
 				<span style="display: table-cell; vertical-align: middle; text-align: center;" class="{{ page == 'stuff' ? 'bold' : '' }}">
 					stuff i made
 				</span>
-			</a>
-			<a href="/" style="display: table; padding: 0px 20px;" class="relative-full-height float-right option home-option">
+			</router-link>
+			<router-link href="/" style="display: table; padding: 0px 20px;" class="relative-full-height float-right option home-option">
 				<span style="display: table-cell; vertical-align: middle; text-align: center;" class="{{ page == 'home' ? 'bold' : '' }}">
 					home
 				</span>
-			</a>
+			</router-link>
 		</div>
 		<img src="/menu.svg">
 		<Menu></Menu>
