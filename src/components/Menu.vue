cc8ba8e0eb3cdf0d994c7ff8f06a3e5c02faff09
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-08 15:07:23 -0600

    Update Menu.vue

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,16 +1,16 @@
 <template>
 	<div class="menu" style="text-align: left; padding: 1.5em 3em; overflow-y: auto; overflow-x: hidden;">
-		<a href="/true-facts">
+		<router-link href="/true-facts">
 			<h2 style="margin-bottom: 0.5em;color: var(--secondary);" class="blue">true facts</h2>
 			true facts about everything
-		</a> <br><br>
-		<a href="/scribble">
+		</router-link> <br><br>
+		<router-link href="/scribble">
 			<h2 style="margin-bottom: 0.5em;color: var(--secondary);" class="blue">scribble</h2>
 			ruins your drawings
-		</a> <br><br>
-		<a href="/car-game">
+		</router-link> <br><br>
+		<router-link href="/car-game">
 			<h2 style="margin-bottom: 0.5em;color: var(--secondary);" class="blue">car game</h2>
 			vroom vroom boom
-		</a>
+		</router-link>
 	</div>
 </template>
