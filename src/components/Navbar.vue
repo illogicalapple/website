8095635adb5262bbdac397104db6f4b6b6d66233
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-08 15:18:14 -0600

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,7 +12,7 @@
 				<span style="display: table-cell; vertical-align: middle; text-align: center;" class="{{ page == 'stuff' ? 'bold' : '' }}">
 					stuff i made
 				</span>
-			</router-link>
+			</a>
 			<router-link to="/" style="display: table; padding: 0px 20px;" class="relative-full-height float-right option home-option">
 				<span style="display: table-cell; vertical-align: middle; text-align: center;" class="{{ page == 'home' ? 'bold' : '' }}">
 					home
