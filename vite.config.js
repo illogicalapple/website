b899c9cebcede6945b4c4a9a4f28fee30f348170
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-09 09:43:08 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const http = require('http')
+const fs = require('fs')
+const httpPort = 80
 
+http.createServer((req, res) => {
+  fs.readFile('index.html', 'utf-8', (err, content) => {
+    if (err) {
+      console.log('We cannot open "index.html" file.')
+    }
+
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    })
+
+    res.end(content)
+  })
+}).listen(httpPort, () => {
+  console.log('Server listening on: http://localhost:%s', httpPort)
+})
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()]
