2cbdd99e1ca54e93c7681b86c7c31d34bcd37554
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-09 10:19:40 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const fs = require('fs')
+
+http.createServer((req, res) => {
+  fs.readFile('index.html', 'utf-8', (err, content) => {
+    if (err) {
+      console.log('We cannot open "index.html" file.')
+    }
+
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    })
+
+    res.end(content)
+  })
+}).listen("/", () => {
+  // stuf
+})
