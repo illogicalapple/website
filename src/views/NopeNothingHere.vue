5dbb3696b542ddb9ffd03fb2dd1282b3d7f53fe7
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-09 09:20:26 -0600

    Update NopeNothingHere.vue

--- src/views/NopeNothingHere.vue
+++ src/views/NopeNothingHere.vue
@@ -1,5 +1,4 @@
 <template>
-	<Navbar></Navbar>
 	<main id="content">
 		<div style="text-align: center; transform: translateY(-50%); position: absolute; top: 50%; width: 100%;">
 			<h1 style="font-size: 20vmin; background: url(/apples.png); background-clip: text; -webkit-background-clip: text; color: transparent; background-position: center 20px;">404</h1>
