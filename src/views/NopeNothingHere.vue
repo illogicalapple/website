22e825cce18fde08b70efdc3a30da78f3ddb6ef6
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-09 11:38:36 -0600

    Update NopeNothingHere.vue

--- src/views/NopeNothingHere.vue
+++ src/views/NopeNothingHere.vue
@@ -2,19 +2,19 @@
 	<main id="content">
 		<div style="text-align: center; transform: translateY(-50%); position: absolute; top: 50%; width: 100%;">
 			<h1 class="404">404</h1>
+			<style scoped>
+				h1.404 {
+					font-size: 20vmin;
+					background: url(/apples.png);
+					background-clip: text;
+					-webkit-background-clip: text;
+					color: transparent;
+					background-position: center;
+					background-size: 3.5em;
+				}
+			</style>
 			<span style="font-size: 1.4em;">evil kumquats ate this page</span>
 		</div>
 		<img src="/burp.png" style="position: absolute; right: 0px; bottom: 0px; transform: translate(-20%, -20%)" />
 	</main>
 </template>
-<style>
-	h1.404 {
-		font-size: 20vmin;
-		background: url(/apples.png);
-		background-clip: text;
-		-webkit-background-clip: text;
-		color: transparent;
-		background-position: center;
-		background-size: 3.5em;
-	}
-</style>
