d7f6390c6fb0ba00f7a9a7791db8dcfcc149a952
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-09 11:42:07 -0600

    Update NopeNothingHere.vue

--- src/views/NopeNothingHere.vue
+++ src/views/NopeNothingHere.vue
@@ -1,7 +1,7 @@
 <template>
 	<main id="content">
 		<div style="text-align: center; transform: translateY(-50%); position: absolute; top: 50%; width: 100%;">
-			<h1 class="404">404</h1>
+			<h1 class="not-found">404</h1>
 			<span style="font-size: 1.4em;">evil kumquats ate this page</span>
 		</div>
 		<img src="/burp.png" style="position: absolute; right: 0px; bottom: 0px; transform: translate(-20%, -20%)" />
