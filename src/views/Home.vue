5ea996521a34b3d14c880195348e4348c2ab4ae6
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-09 12:40:40 -0600

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,12 @@
 <template>
-	<data data-home></data>
 	<main id="content">
-		<div class="text" style="position: absolute; transform: translateY(-50%); top: calc(50% + 50px); left: 10vw; width: calc(80vw - (100vh - 50px) / 1.509765625); font-size: 1.4em;">
-			<h1 class="red"> Hello. </h1>
-			<p style="padding: 0px; margin: 0px;">
-				My name is <span class="red">
-					illogicalapple
-				</span> and I am an <span class="red">
-					apple. 
-				</span>
-				I started making <router-link to="true-facts" style="color: var(--orange);">true-facts</router-link> in <time datetime="2020-12">
-					December 2020,
-				</time> and I made some other fun <router-link to="my-stuff" style="color: var(--orange);">stuff</router-link> later.
-				I really like making stuff.
-				<br> <br> Hope you enjoy!
+		<img src="apples-cropped.png" style="height: 30vw; top: 50%; position: absolute; transform: translateY(-50%); left: 15vw; max-width: 40vh; max-height: 40vh;">
+		<div class="text" style="left: calc(30vw + min(30vw, 40vh)); position: absolute; width: 25vw; top: 50%; transform: translateY(-50%);">
+			<h1> hi, <span class="blue">it's me.</span></h1>
+			<p style="font-size: 1.4em;">
+				vote apples over kumquats <br>
+				let's go apples!
 			</p>
 		</div>
-		<img src="/apple-image.jpg" style="position: absolute; bottom: 0px; right: 0px; height: calc(100vh - 50px); z-index: -1;" class="apple" alt="Apple">
 	</main>
 </template>
