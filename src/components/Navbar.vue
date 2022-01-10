04b1019a792c06b1253ada5acb6ca02a0b042c52
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-10 10:56:55 -0600

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -26,7 +26,9 @@
 				<router-link :to="page.url">
 					<h2 style="margin-bottom: 0.5em;color: var(--secondary);" class="blue">{{ page.title }}</h2>
 					{{ page.desc }}
-				</router-link> <span v-if="index !== pages.length"><br><br></span>
+				</router-link>
+				<span v-if="index < pages.length"><br><br></span>
+				<span v-else><!-- haha no line break sucker --></span>
 			</span>
 		</div>
 	</nav>
