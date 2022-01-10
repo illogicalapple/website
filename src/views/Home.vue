98014bac7a191e03d85e5c478988bcc4caa8640f
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-09 18:07:46 -0600

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
 	<main id="content">
 		<img src="/apples-cropped.png" style="height: 30vw; top: 50%; position: absolute; transform: translateY(-50%); left: 15vw; max-width: 40vh; max-height: 40vh;">
 		<div class="text" style="left: calc(30vw + min(30vw, 40vh)); position: absolute; width: 25vw; top: 50%; transform: translateY(-50%);">
-			<h1> hi, <span class="home-blue-underline">it's me.</span></h1>
+			<h1> hi, <span class="home-blue-underline" style="position: relative;">it's me.</span></h1>
 			<p style="font-size: 1.4em;">
 				vote apples over kumquats <br>
 				let's go apples!
