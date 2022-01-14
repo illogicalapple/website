e3d128e4109862b7b8124b47872587dd7aa9ca12
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-13 20:22:11 -0600

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
-	<main id="content">
-		<img src="/apples-cropped.png" style="height: 30vw; top: 50%; position: absolute; transform: translateY(-50%); left: 15vw; max-width: 40vh; max-height: 40vh;">
-		<div class="text" style="left: calc(30vw + min(30vw, 40vh)); position: absolute; width: 25vw; top: 50%; transform: translateY(-50%);">
+	<main id="content" style="--forty: calc(0.4 * var(--height));">
+		<img src="/apples-cropped.png" style="height: 30vw; top: 50%; position: absolute; transform: translateY(-50%); left: 15vw; max-width: var(--forty); max-height: var(--forty);">
+		<div class="text" style="left: calc(30vw + min(30vw, var(--forty))); position: absolute; width: 25vw; top: 50%; transform: translateY(-50%);">
 			<h1> hi, <span class="home-blue-underline" style="position: relative;">it's me.</span></h1>
 			<p style="font-size: 1.4em;">
 				vote apples over kumquats <br>
