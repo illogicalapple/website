dd7a6d334399ca65ee88863f985a99d8ae58324f
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-13 18:21:42 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,15 @@
 <script setup>
 	import { ref } from "vue";
 	import Navbar from "./components/Navbar.vue";
+	const onResize = function onResize(event) {
+		event.target.document.body.style.setProperty("--height", event.target.innerHeight)
+	}
+	const mounted = function mounted() {
+		window.addEventListener("resize", this.onResize);
+		window.addEventListener("load", this.onResize);
+	}
+	const beforeDestroy = function beforeDestroy() {
+		window.removeEventListener("resize", this.onResize);
+		window.removeEventListener("load", this.onResize);
+	}
 </script>
