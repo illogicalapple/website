bdb586afe8b3597fca5d7752ac31f236aa10bd69
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-16 08:25:13 -0600

    Create Blog.vue

---
+++ src/views/Blog.vue
@@ -0,0 +1,24 @@
+<template>
+	<main id="content" style="--forty: calc(0.4 * var(--height));">
+		<h1 class="blog blog-title">blog</h1>
+		<div class="blog posts">
+			<div class="blog post" v-for="post in posts">
+				<h2 :data-title="post.title" class="blog blog-post title" :data-transparent="!posts">{{ post.title }}</h2>
+				<p class="blog blog-post desc" :data-transparent="!posts">{{ post.desc }}</p>
+				<span class="blog blog-post creator" :data-transparent="!posts">By {{ post.author }}</span> &bull;
+				<span class="blog blog-post date" :data-transparent="!posts">{{ post.created }}</span>
+			</div>
+		</div>
+	</main>
+</template>
+<script setup>
+	const _posts = await fetch("../blog/entries.json").then(response => response.json());
+	const posts = _posts.sort((a, b) => new Date(b.created).getTime() - new Date(a.created).getTime()); // newest to oldest
+</script>
+<style scoped>
+	.blog-post[data-transparent] {
+		background-color: lightgray;
+		border-radius: 5px;
+		color: transparent;
+	}
+</style>
