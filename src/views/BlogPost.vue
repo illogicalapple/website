c9ac1cfc4bcaf0fd260576c9ded12822e1fbfd2e
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-17 08:50:06 -0600

    Update BlogPost.vue

--- src/views/BlogPost.vue
+++ src/views/BlogPost.vue
@@ -24,7 +24,7 @@
 		created: "not found"
 	});
 	const post = ref("");
-	window.addEventListener("load", function() {
+	onMounted(function() {
 		fetch("/blog/entries.json").then(r => r.json()).then(function(r) {
 			postData.value = r.find(e => e.file == postName);
 			document.title = postData.value.title;
