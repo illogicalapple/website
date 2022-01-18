dd96549361138daa8624a5377e609593da944d08
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-17 18:04:25 -0600

    Update BlogPost.vue

--- src/views/BlogPost.vue
+++ src/views/BlogPost.vue
@@ -22,30 +22,34 @@
 	});
 	const post = ref("");
 	onMounted(async function() {
-		const asdf = await fetch("/blog/entries.json").then(r => r.json());
-		postData.value = asdf.find(e => e.file == postName);
-		document.title = postData.value.title;
-		const el = document.createElement("meta");
-		el.setAttribute("name", "description");
-		el.setAttribute("content", postData.value.desc);
-		document.head.appendChild(el);
-		const el2 = document.createElement("meta");
-		el2.setAttribute("name", "author");
-		el2.setAttribute("content", postData.value.author);
-		document.head.appendChild(el2);
-		if(postData.value == undefined) {
-			found.value = false;
-		}
-		if(found.value) {
-			const hjkl = await fetch(`/blog/${postName}.md`).then(e => e.text());
-			post.value = marked.parse(hjkl);
-		} else {
-			document.title = "404: not found";
+		try {
+			const asdf = await fetch("/blog/entries.json").then(r => r.json());
+			postData.value = asdf.find(e => e.file == postName);
+			document.title = postData.value.title;
 			const el = document.createElement("meta");
-			el.setAttribute("name", "robots");
-			el.setAttribute("content", "noindex");
+			el.setAttribute("name", "description");
+			el.setAttribute("content", postData.value.desc);
 			document.head.appendChild(el);
-			this.$router.push("/not-found?from=blog");
+			const el2 = document.createElement("meta");
+			el2.setAttribute("name", "author");
+			el2.setAttribute("content", postData.value.author);
+			document.head.appendChild(el2);
+			if(postData.value == undefined) {
+				found.value = false;
+			}
+			if(found.value) {
+				const hjkl = await fetch(`/blog/${postName}.md`).then(e => e.text());
+				post.value = marked.parse(hjkl);
+			} else {
+				document.title = "404: not found";
+				const el = document.createElement("meta");
+				el.setAttribute("name", "robots");
+				el.setAttribute("content", "noindex");
+				document.head.appendChild(el);
+				this.$router.push("/not-found?from=blog");
+			}
+		} catch(error) {
+			alert(error.message);
 		}
 	});
 </script>
