f3b0e65273fbbd63f0722b25f5caf757df405c2d
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-02-09 07:41:12 -0600

    Update check.js

--- src/modules/spelchek/check.js
+++ src/modules/spelchek/check.js
@@ -3,6 +3,9 @@ export default class Spelchek() {
 		this.text = text;
 		this._text = text;
 		this._vowels = "aeiou";
+		this._alphabet = "abcdefghijklmnopqrstuvwxyz";
+		this._alphabet += this._alphabet.toUpperCase();
+		this._punctuation = ",.?/<>;:'\"[]{}\\|=+-_)(*&^%$#@!~`"
 		this.warp = {
 			_sort(chekArray) {
 				let priorities = [];
@@ -17,6 +20,14 @@ export default class Spelchek() {
 			vowel(priority) {
 				if(!priority) this.text = [...this.text].map(element => this._vowels.includes(element) ? this._vowels(Math.floor(Math.random() * 5)) : element).join("");
 				return { PRIORITY: 200 }
+			},
+			typo(priority) {
+				if(!priority) this.text = [...this.text].map(element => this._alphabet.includes(element) && Math.random() < 0.09 ? this._alphabet[Math.floor(Math.random() * 54)] : element)
+				return { PRIORITY: 201 }
+			},
+			punctuation(priority) {
+				if(!priority) this.text = [...this.text].filter(e => !this._punctuation.includes(e)).forEach((e, i, a) => Math.random() < 0.09 && a.splice(i, 0, this._punctuation[Math.random() * this._punctuation.length]))
+				return { PRIORITY: 199 }
 			}
 		}
 	}
@@ -26,9 +37,10 @@ export default class Spelchek() {
 	}
 	static get cheks() {
 		return [
-			"vowel", // replace random vowels
-			"typo", // replace keys (randomly) to keys slightly different
+			"vowel", // replace random vowels DONE
+			"typo", // replace keys (randomly) to keys different DONE
 			"snake", // eat random parts
+			"punctuation", // punctuate and delete punctuate DONE
 			"adverb", // put in random adverbs
 			"noun" // put in random nouns
 		];
