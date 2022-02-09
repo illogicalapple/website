d9cfeb9249a75b612a096d24ad305e77bdcb76a2
irrationalapple <97689435+irrationalapple@users.noreply.github.com>
2022-02-09 10:09:02 -0600

    Update check.js

--- src/modules/spelchek/check.js
+++ src/modules/spelchek/check.js
@@ -18,17 +18,22 @@ export default class Spelchek() {
 				return priorities.sort((first, second) => first.PRIORITY - second.PRIORITY)
 			},
 			vowel(priority) {
-				if(!priority) this.text = [...this.text].map(element => this._vowels.includes(element) ? this._vowels(Math.floor(Math.random() * 5)) : element).join("");
+				if(!priority) this.text = this.text.split("").map(element => this._vowels.includes(element) ? this._vowels(Math.floor(Math.random() * 5)) : element).join("");
 				return { PRIORITY: 200 }
 			},
 			typo(priority) {
-				if(!priority) this.text = [...this.text].map(element => this._alphabet.includes(element) && Math.random() < 0.09 ? this._alphabet[Math.floor(Math.random() * 54)] : element)
+				if(!priority) this.text = this.text.split("").map(element => this._alphabet.includes(element) && Math.random() < 0.09 ? this._alphabet[Math.floor(Math.random() * 54)] : element);
 				return { PRIORITY: 201 }
 			},
 			punctuation(priority) {
-				if(!priority) this.text = [...this.text].filter(e => !this._punctuation.includes(e)).forEach((e, i, a) => Math.random() < 0.09 && a.splice(i, 0, this._punctuation[Math.random() * this._punctuation.length]))
+				if(!priority) this.text = this.text.split("").filter(e => !this._punctuation.includes(e)).forEach((e, i, a) => Math.random() < 0.09 && a.splice(i, 0, this._punctuation[Math.random() * this._punctuation.length]));
 				return { PRIORITY: 199 }
-			}
+			},
+			snake(priority) {
+				if(!priority) this.text = this.text.split("").filter(e => Math.random() > 0.9);
+				return { PRIORITY: 202 }
+			},
+			
 		}
 	}
 	go(cheks) {
@@ -40,9 +45,9 @@ export default class Spelchek() {
 			"vowel", // replace random vowels DONE
 			"typo", // replace keys (randomly) to keys different DONE
 			"snake", // eat random parts
-			"punctuation", // punctuate and delete punctuate DONE
-			"adverb", // put in random adverbs
-			"noun" // put in random nouns
+			"punctuation" // , // punctuate and delete punctuate DONE
+			// "adverb", // put in random adverbs
+			// "noun" // put in random nouns
 		];
 	}
 }
