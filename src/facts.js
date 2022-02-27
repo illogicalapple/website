37bbdbbea3ccb5108b3acac4afe4cef6c7deb9e0
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-02-27 12:19:01 -0600

    Create facts.js

---
+++ src/facts.js
@@ -0,0 +1,21 @@
+let facts = [
+	"vegetables stink",
+	"potatoes suck",
+	"dogeminer increases your iq",
+	"banks are insecure: they get robbed so often in stories",
+	"minecraft gud roblox gud fortnite bad",
+	"99% of veggies are toxic",
+	"eggplants are terrible",
+	"homework was once a form of torture",
+	"working destroys your brain",
+	"dinosaurs have been around for nearly 300 years",
+	"ants are an endangered species",
+	"who let the dogs out",
+	"apples are the best",
+	"water is way overrated",
+	"the sun orbits the earth",
+	"i am the center of the universe",
+	"i am a time traveler",
+	"goguardian is the worst thing ever created"
+];
+export default facts;
