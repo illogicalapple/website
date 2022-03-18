7b4163310962eb38ede263efa0dd6bdb8e9644c7
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-03-18 07:47:01 -0500

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,7 +3,7 @@
     <button @click="login()">Log in</button>
   </main>
 </template>
-<script>
+<script setup>
   import { useAuth0 } from '@auth0/auth0-vue';
   const { loginWithRedirect, user, isAuthenticated } = useAuth0();
   const login = function() {
