d561797e81beb3ecdf71306edbf13aae0664b2ee
irrationalapple <97689435+irrationalapple@users.noreply.github.com>
2022-03-18 12:58:58 -0500

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,10 @@
 <template>
   <main id="content">
-    <button @click="useAuth0().loginWithRedirect()">Log in</button>
+    <button @click="loginWithRedirect()">Log in</button>
   </main>
 </template>
 <script setup>
-  import { useAuth0 } from '@auth0/auth0-vue';
+  import { useAuth0 } from '@auth0/auth0-vue'
+  const { loginWithRedirect } = useAuth0();
+  alert(loginWithRedirect.toString());
 </script>
