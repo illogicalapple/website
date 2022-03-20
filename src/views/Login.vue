47f72f88ab9e02a3d09dfa721a055f76d0732773
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-03-20 11:25:12 -0500

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,21 +1,5 @@
 <template>
   <main id="content">
-    <button @click="login">Log in</button>
+    <router-link to="_login">log in</router-link>
   </main>
 </template>
-<script>
-  import { useAuth0 } from '@auth0/auth0-vue';
-
-  export default {
-    setup() {
-      console.log(useAuth0());
-      const { loginWithRedirect } = useAuth0();
-
-      return {
-        login: () => {
-          loginWithRedirect();
-        }
-      };
-    }
-  };
-</script>
