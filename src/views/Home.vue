a47e7012b86bfaf551083888739ad4daf776be38
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-03-31 18:38:40 -0500

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,12 @@
 <template>
-	<main id="content" style="--forty: calc(0.4 * var(--height));">
-		<img src="/apples-cropped.png" style="height: 30vw; top: 50%; position: absolute; transform: translateY(-50%); left: 15vw; max-width: var(--forty); max-height: var(--forty);">
-		<div class="text" style="left: calc(30vw + min(30vw, var(--forty))); position: absolute; width: 25vw; top: 50%; transform: translateY(-50%);">
-			<h1> hi, <span class="home-blue-underline" style="position: relative;">it's me.</span></h1>
-			<p style="font-size: 1.4em;">
-				pray for ukraine
-			</p>
-		</div>
-	</main>
+	<span style="color: purple">
+		<h1>this website has been taken down</h1>
+		<p>you may find everything here at <a href="https://gh.illogicalapple.com">gh.illogicalapple.com</a></p>
+		<p>this has been taken down for <a href="javascript:alert('april fools');document.body.innerHTML = '<h1 style=\'font-size: 4em\'>LOL</h1>'">political reasons</a></p>
+	</span>
 </template>
+<style>
+	body {
+		background-color: yellow;
+	}
+</style>
